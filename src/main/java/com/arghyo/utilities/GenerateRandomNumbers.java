59f19386adeb6c8ed443d659578e3e3bb8a1ce9c
Arghyo <arghyo@gmail.com>
2017-05-10 20:09:27 +0000

    Generate random integers

--- src/main/java/com/arghyo/utilities/GenerateRandomNumbers.java
+++ src/main/java/com/arghyo/utilities/GenerateRandomNumbers.java
@@ -1,8 +1,31 @@
-package org.eclipse.che.examples;
+package com.arghyo.utilities;
 
-public class HelloWorld {
+import java.util.Random;
+
+public class GenerateRandomNumbers {
     public static void main(String... argvs) {
-        String a = "Che";
-        System.out.println("Hello World " + a + "!");
+        
+        GenerateRandomNumbers obj = new GenerateRandomNumbers();
+        int[] returnIntArray = obj.generateNRandomNumbersInRange(-10, 10, 10);
+        
+        for(int i=0; i<10; i++){
+            System.out.println(returnIntArray[i]);
+        }
+        
+        
+        
+    }
+    
+    public int[] generateNRandomNumbersInRange(int min, int max, int n){
+        int [] intArray = new int[n];
+        
+        Random rand = new Random();
+        
+        for(int i=0; i < n; i++){
+            intArray[i] = rand.nextInt(max + 1 - min) + min;
+        }
+        return intArray;
+        
+        
     }
 }
